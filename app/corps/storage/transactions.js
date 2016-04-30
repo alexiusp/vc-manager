8238320e1d8a72c300416f5078719d3b3bade617
Aleksei "AlexiuS" Podgaev <podgaev@gmail.com>
2016-04-30 22:07:22 +0200

    multiple items transaction and clear storage transaction introduced
    TODO: implement this transactions in supply list component

--- app/corps/storage/transactions.js
+++ app/corps/storage/transactions.js
@@ -6,7 +6,7 @@ System.register([], function(exports_1, context_1) {
         function __() { this.constructor = d; }
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
-    var TransactionType, TransactionDirection, BaseTransaction, TransferItemTransaction, InvestTransaction, ClearStorageTransaction, SellItemTransaction;
+    var TransactionType, TransactionDirection, BaseTransaction, ItemsPackageTransaction, ItemsTransaction, ClearStorageTransaction, TransferItemTransaction, InvestTransaction, SellItemTransaction;
     return {
         setters:[],
         execute: function() {
@@ -24,6 +24,7 @@ System.register([], function(exports_1, context_1) {
                 TransactionDirection[TransactionDirection["ToCorporation"] = 3] = "ToCorporation";
             })(TransactionDirection || (TransactionDirection = {}));
             exports_1("TransactionDirection", TransactionDirection);
+            // base class for all transactions
             BaseTransaction = (function () {
                 function BaseTransaction(type, direction, business) {
                     this.type = type;
@@ -39,9 +40,99 @@ System.register([], function(exports_1, context_1) {
                         return false;
                     return true;
                 };
+                BaseTransaction.prototype.getTitle = function () {
+                    return "Transaction: ";
+                };
                 return BaseTransaction;
             }());
             exports_1("BaseTransaction", BaseTransaction);
+            // intermediate class to work with items array
+            ItemsPackageTransaction = (function (_super) {
+                __extends(ItemsPackageTransaction, _super);
+                function ItemsPackageTransaction(type, direction, business) {
+                    _super.call(this, type, direction, business);
+                    this._items = [];
+                }
+                Object.defineProperty(ItemsPackageTransaction.prototype, "items", {
+                    get: function () { return this._items; },
+                    enumerable: true,
+                    configurable: true
+                });
+                ItemsPackageTransaction.prototype.addItem = function (item) {
+                    this._items.push(item);
+                };
+                ItemsPackageTransaction.prototype.removeItem = function (item) {
+                    var iArr = [];
+                    for (var _i = 0, _a = this._items; _i < _a.length; _i++) {
+                        var i = _a[_i];
+                        if (i.item.ItemType.id !== item.item.ItemType.id)
+                            iArr.push(i);
+                    }
+                    this._items = iArr;
+                };
+                // replaces an item - used to change amount
+                ItemsPackageTransaction.prototype.replaceItem = function (item) {
+                    var iArr = [];
+                    for (var _i = 0, _a = this._items; _i < _a.length; _i++) {
+                        var i = _a[_i];
+                        if (i.item.ItemType.id !== item.item.ItemType.id)
+                            iArr.push(i);
+                        else
+                            iArr.push(item);
+                    }
+                    this._items = iArr;
+                };
+                ItemsPackageTransaction.prototype.hasItem = function (item) {
+                    for (var _i = 0, _a = this._items; _i < _a.length; _i++) {
+                        var i = _a[_i];
+                        if ((item.item.ItemType.id === i.item.ItemType.id) && (item.amount === i.amount))
+                            return true;
+                    }
+                    return false;
+                };
+                ItemsPackageTransaction.prototype.isEqual = function (target) {
+                    if (!_super.prototype.isEqual.call(this, target))
+                        return false;
+                    if (target instanceof ItemsTransaction) {
+                        for (var _i = 0, _a = target.items; _i < _a.length; _i++) {
+                            var t = _a[_i];
+                            if (!this.hasItem(t))
+                                return false;
+                        }
+                        return true;
+                    }
+                    else
+                        return false;
+                };
+                return ItemsPackageTransaction;
+            }(BaseTransaction));
+            // final transaction class to work with transfer of multiple items
+            ItemsTransaction = (function (_super) {
+                __extends(ItemsTransaction, _super);
+                function ItemsTransaction(direction, business) {
+                    _super.call(this, TransactionType.Transfer, direction, business);
+                }
+                ItemsTransaction.prototype.getTitle = function () {
+                    var source = (this.direction === TransactionDirection.FromCompany) ? this.business.name : "Corporation";
+                    var target = (this.direction === TransactionDirection.FromCorporation) ? this.business.name : "Corporation";
+                    return _super.prototype.getTitle.call(this) + "Transfer of " + this.items.length + " item packages from " + source + " to " + target;
+                };
+                return ItemsTransaction;
+            }(ItemsPackageTransaction));
+            exports_1("ItemsTransaction", ItemsTransaction);
+            ClearStorageTransaction = (function (_super) {
+                __extends(ClearStorageTransaction, _super);
+                function ClearStorageTransaction(direction, business) {
+                    _super.call(this, TransactionType.ClearStorage, direction, business);
+                }
+                ClearStorageTransaction.prototype.getTitle = function () {
+                    var source = (!!this.business) ? this.business.name : "Corporation";
+                    return _super.prototype.getTitle.call(this) + "Clear storage of " + source + " which contains " + this.items.length + " item packages";
+                };
+                return ClearStorageTransaction;
+            }(ItemsPackageTransaction));
+            exports_1("ClearStorageTransaction", ClearStorageTransaction);
+            // single item transfer transaction - DEPRECATED
             TransferItemTransaction = (function (_super) {
                 __extends(TransferItemTransaction, _super);
                 function TransferItemTransaction(amount, item, direction, business) {
@@ -60,6 +151,11 @@ System.register([], function(exports_1, context_1) {
                     else
                         return false;
                 };
+                TransferItemTransaction.prototype.getTitle = function () {
+                    var source = (this.direction === TransactionDirection.FromCompany) ? this.business.name : "Corporation";
+                    var target = (this.direction === TransactionDirection.FromCorporation) ? this.business.name : "Corporation";
+                    return _super.prototype.getTitle.call(this) + "Transfer of " + this.amount + " " + this.item.ItemType.name + " from " + source + " to " + target;
+                };
                 return TransferItemTransaction;
             }(BaseTransaction));
             exports_1("TransferItemTransaction", TransferItemTransaction);
@@ -69,17 +165,12 @@ System.register([], function(exports_1, context_1) {
                     _super.call(this, TransactionType.Invest, direction, business);
                     this.money = money;
                 }
+                InvestTransaction.prototype.getTitle = function () {
+                    return _super.prototype.getTitle.call(this) + "Invest " + this.money + " vDollars to " + this.business.name;
+                };
                 return InvestTransaction;
             }(BaseTransaction));
             exports_1("InvestTransaction", InvestTransaction);
-            ClearStorageTransaction = (function (_super) {
-                __extends(ClearStorageTransaction, _super);
-                function ClearStorageTransaction(direction, business) {
-                    _super.call(this, TransactionType.ClearStorage, direction, business);
-                }
-                return ClearStorageTransaction;
-            }(BaseTransaction));
-            exports_1("ClearStorageTransaction", ClearStorageTransaction);
             SellItemTransaction = (function (_super) {
                 __extends(SellItemTransaction, _super);
                 function SellItemTransaction(amount, item, money, direction, business) {
@@ -98,6 +189,10 @@ System.register([], function(exports_1, context_1) {
                     }
                     else
                         return false;
+                };
+                SellItemTransaction.prototype.getTitle = function () {
+                    var source = (!!this.business) ? this.business.name : "Corporation";
+                    return _super.prototype.getTitle.call(this) + "Sell " + this.amount + " of " + this.item.ItemType.name + " for " + this.money + " vDollars from " + source;
                 };
                 return SellItemTransaction;
             }(BaseTransaction));
--- app/corps/storage/transactions.ts
+++ app/corps/storage/transactions.ts
@@ -18,6 +18,8 @@ export interface IBaseTransaction {
 	type			: TransactionType;
 	direction	: TransactionDirection;
 	business	: BaseBusiness;
+	isEqual(target : IBaseTransaction) : boolean;
+	getTitle() : string;
 }
 export interface ICountableTransaction {
 	amount	: number;
@@ -28,7 +30,9 @@ export interface IMoneyTransaction {
 export interface IItemTransaction {
 	item : BaseStorageElement
 }
-
+export interface IItemPackage extends IItemTransaction, ICountableTransaction {
+}
+// base class for all transactions
 export class BaseTransaction implements IBaseTransaction {
 	constructor(public type			: TransactionType,
 		public direction	: TransactionDirection,
@@ -39,7 +43,73 @@ export class BaseTransaction implements IBaseTransaction {
 		if(this.business.id !== target.business.id) return false;
 		return true;
 	}
+	public getTitle() : string {
+		return "Transaction: ";
+	}
 }
+// intermediate class to work with items array
+class ItemsPackageTransaction extends BaseTransaction {
+	private _items : IItemPackage[];
+	constructor(
+		type			: TransactionType,
+		direction	: TransactionDirection,
+		business	: BaseBusiness) {
+		super(type, direction, business);
+		this._items = [];
+	}
+	public get items() { return this._items; }
+	public addItem(item : IItemPackage) {
+		this._items.push(item);
+	}
+	public removeItem(item : IItemPackage) {
+		let iArr : IItemPackage[] = [];
+		for(let i of this._items) if(i.item.ItemType.id !== item.item.ItemType.id) iArr.push(i);
+		this._items = iArr;
+	}
+	// replaces an item - used to change amount
+	public replaceItem(item : IItemPackage) {
+		let iArr : IItemPackage[] = [];
+		for(let i of this._items)
+			if(i.item.ItemType.id !== item.item.ItemType.id) iArr.push(i);
+			else iArr.push(item);
+		this._items = iArr;
+	}
+	public hasItem(item : IItemPackage) {
+		for(let i of this._items)
+			if((item.item.ItemType.id === i.item.ItemType.id) && (item.amount === i.amount)) return true;
+		return false;
+	}
+	public isEqual(target : BaseTransaction) : boolean {
+		if(!super.isEqual(target)) return false;
+		if(target instanceof ItemsTransaction) {
+			for(let t of target.items) if(!this.hasItem(t)) return false;
+			return true;
+		} else return false;
+	}
+}
+// final transaction class to work with transfer of multiple items
+export class ItemsTransaction extends ItemsPackageTransaction {
+	constructor(direction	: TransactionDirection,
+		business	: BaseBusiness) {
+		super(TransactionType.Transfer, direction, business);
+	}
+	public getTitle() : string {
+		let source = (this.direction === TransactionDirection.FromCompany) ? this.business.name : "Corporation";
+		let target = (this.direction === TransactionDirection.FromCorporation) ? this.business.name : "Corporation";
+		return super.getTitle() + "Transfer of " + this.items.length + " item packages from " + source + " to " + target;
+	}
+}
+export class ClearStorageTransaction extends ItemsPackageTransaction {
+	constructor(direction	: TransactionDirection,
+		business	: BaseBusiness) {
+		super(TransactionType.ClearStorage, direction, business);
+	}
+	public getTitle() : string {
+		let source = (!!this.business) ? this.business.name : "Corporation";
+		return super.getTitle() + "Clear storage of " + source + " which contains " + this.items.length + " item packages";
+	}
+}
+// single item transfer transaction - DEPRECATED
 export class TransferItemTransaction extends BaseTransaction implements ICountableTransaction, IItemTransaction {
 	constructor(public amount	: number,
 		public item    : BaseStorageElement,
@@ -54,6 +124,11 @@ export class TransferItemTransaction extends BaseTransaction implements ICountab
 			return true;
 		} else return false;
 	}
+	public getTitle() : string {
+		let source = (this.direction === TransactionDirection.FromCompany) ? this.business.name : "Corporation";
+		let target = (this.direction === TransactionDirection.FromCorporation) ? this.business.name : "Corporation";
+		return super.getTitle() + "Transfer of " + this.amount + " " + this.item.ItemType.name + " from " + source + " to " + target;
+	}
 }
 export class InvestTransaction extends BaseTransaction implements IMoneyTransaction {
 	constructor(public money	: number,
@@ -61,11 +136,8 @@ export class InvestTransaction extends BaseTransaction implements IMoneyTransact
 		business	: BaseBusiness) {
 		super(TransactionType.Invest, direction, business);
 	}
-}
-export class ClearStorageTransaction extends BaseTransaction {
-	constructor(direction	: TransactionDirection,
-		business	: BaseBusiness) {
-		super(TransactionType.ClearStorage, direction, business);
+	public getTitle() : string {
+		return super.getTitle() + "Invest " + this.money + " vDollars to " + this.business.name;
 	}
 }
 export class SellItemTransaction extends BaseTransaction implements IMoneyTransaction, ICountableTransaction {
@@ -82,5 +154,9 @@ export class SellItemTransaction extends BaseTransaction implements IMoneyTransa
 				if(this.item.ItemType.id !== target.item.ItemType.id) return false;
 				return true;
 			} else return false;
+		}
+		public getTitle() : string {
+			let source = (!!this.business)? this.business.name : "Corporation";
+			return super.getTitle() + "Sell " + this.amount + " of " + this.item.ItemType.name + " for " + this.money + " vDollars from " + source;
 		}
 }
